08ee321aeef35e09e07a390747f8f70e2dc15864
Uddhav Paudel <paudeluddhav.er@gmail.com>
2025-05-05 08:14:31 +0100

    feat! : Testing & Linting implementation.

--- fsapp/src/App.jsx
+++ fsapp/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState, useEffect } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import axios from "axios"; // Import Axios for API requests
 const apiUrl = import.meta.env.VITE_API_URL;
